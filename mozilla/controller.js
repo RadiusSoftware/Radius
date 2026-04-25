f51e59f4181c1c93d7dbf8c91175580d8a5a8f69
Christoph Wittmann <notsuresecretary@gmail.com>
2026-04-25 12:33:34 -0700

    Got the basic binding algorithm tested.

--- mozilla/controller.js
+++ mozilla/controller.js
@@ -33,46 +33,33 @@ singleton(class Controller extends Emitter {
         super();
         this.data = new WeakMap();
         this.nodes = new WeakMap();
+        this.bindingsByDotted = {};
+        this.bindingsByDocElement = new WeakMap();
     }
 
-    bindAttributeExistence(docElement, attributeName, arg) {
-        // TODO ************************************************************************
-        // TODO ************************************************************************
-    }
-
-    bindAttributeValue(docElement, attributeName, arg) {
+    bindAttrExists(docElement, attributeName, ref) {
         // TODO ************************************************************************
         // TODO ************************************************************************
     }
 
-    bindFunction(docElement, func, arg) {
+    bindAttrValue(docElement, attributeName, ref) {
         // TODO ************************************************************************
         // TODO ************************************************************************
     }
 
-    bindMethod(docElement, methodName, arg) {
+    bindMethod(docElement, methodName, ref) {
         // TODO ************************************************************************
         // TODO ************************************************************************
     }
 
-    bindInner(docElement, arg) {
+    bindInner(docElement, ref) {
         // TODO ************************************************************************
         // TODO ************************************************************************
     }
 
-    bindProperty(docElement, property, arg) {
-        let dataEntry = this.getData(docElement);
-
-        if (dataEntry) {
-            let expr = arg instanceof Expr ? arg : mkControllerExpr(docElement.getRdsBind());
-
-            for (let dependency of expr.getDependencies()) {
-                if (dependency.type == 'controller') {
-                    // TODO ************************************************************************
-                    // TODO ************************************************************************
-                    console.log(dependency);
-                }
-            }
+    bindProperty(docElement, property, ref) {
+        if (typeof property == 'string' && property.trim() != '') {
+            this.setBinding(docElement, ref, 'property', property);
         }
 
         return this;
@@ -147,20 +134,14 @@ singleton(class Controller extends Emitter {
     }
 
     getDataShape(docElement, dotted) {
-        /*
         let shape = null;
-        let array = RdsText.split(dotted, '.');
 
-        if (array.length) {
-            shape = this.shapes[array[0]];
-
-            if (shape && array.length > 1) {
-                shape = shape.get(array.slice(1).join('.'));
-            }
+        if (this.data.has(docElement)) {
+            let data = this.data.get(docElement);
+            shape = data.shape.get(dotted);
         }
 
         return shape;
-        */
     }
 
     getDataValue(docElement, dotted) {
@@ -221,9 +202,32 @@ singleton(class Controller extends Emitter {
         }
     }
 
+    setBinding(docElement, ref, type, name) {
+        let dataEntry = this.getData(docElement);
+
+        if (dataEntry) {
+            let expr;
+
+            if (typeof ref == 'string' && ref.trim() != '') {
+                expr = mkControllerExpr(ref)
+            }
+            else if (ref instanceof Expr) {
+                expr = ref;
+            }
+
+            if (expr) {
+                for (let dependency of expr.getDependencies()) {
+                    if (dependency.type == 'controller') {
+                        let binding = mkControllerBinding(docElement, dependency.dotted, type, name);
+                        console.log(binding);
+                    }
+                }
+            }
+        }
+    }
+
     setDataValue(docElement, dotted, newValue) {
-        /*
-        let shape = this.getShape(dotted);
+        let shape = this.getDataShape(docElement, dotted);
 
         if (shape) {
             if (shape.verify(newValue)) {
@@ -238,7 +242,13 @@ singleton(class Controller extends Emitter {
                 }
                 else {
                     Data.set(this.data, dotted, newValue);
-                    this.updated(dotted, oldValue, newValue);
+
+                    this.emit({
+                        name: 'Set',
+                        dotted: dotted,
+                        oldValue: oldValue,
+                        newValue: newValue,
+                    });
                 }
             }
         }
@@ -251,26 +261,6 @@ singleton(class Controller extends Emitter {
         }
 
         return this;
-        */
-    }
-
-    updated(docElement, dotted, oldValue, newValue) {
-        /*
-        let bindings = this.bindings[dotted];
-
-        if (bindings) {
-            for (let binding of bindings) {
-                entanglement.broadcast(dotted);
-            }
-        }
-        
-        this.emit({
-            name: 'Set',
-            dotted: dotted,
-            oldValue: oldValue,
-            newValue: newValue,
-        });
-        */
     }
 });
 
@@ -317,14 +307,137 @@ define(class ControllerExpr extends Expr {
 
     getDependencies() {
         return [{
-            expr: this,
             type: 'controller',
-            get: async () => Controller.get(this.dotted),
-            set: async value => Controller.set(this.dotted, value),
+            expr: this,
+            dotted: this.dotted,
         }];
     }
 
     getShapes() {
         return [ mkRdsShape(StringType) ];
+    }
+});
+
+
+/*****
+*****/
+define(class ControllerBinding {
+    constructor(docElement, dotted, type, name) {
+        this.docElement = docElement;
+        this.dotted = dotted;
+        this.inner = false;
+        this.valid = false;
+        this.enabled = true;
+        
+        if (type == 'inner') {
+            this.valid = true;
+            this.inner = true;
+        }
+        else if (type in { attrValue:0, attrExists:0, property:0 }) {
+            if (typeof name == 'string' && name != '') {
+                this.valid = true;
+                this.type = type;
+                this.name = name;
+            }
+        }
+        else if (type == 'method') {
+            if (typeof name == 'string' && name != '') {
+                if (name in docElement) {
+                    this.valid = true;
+                    this.type = type;
+                    this.name = name;
+                }
+            }
+        }
+
+        if (this.valid) {
+            let binding = ControllerBinding.get(this);
+            console.log(binding);
+            
+            if (binding) {
+                return binding;
+            }
+
+            let byDocElement = Controller.bindingsByDocElement.get(this.docElement);
+            
+            if (!byDocElement) {
+                byDocElement = {
+                    docElement: this.docElement,
+                    bindings: [],
+                };
+
+                Controller.bindingsByDocElement.set(this.docElement, byDocElement);
+            }
+
+            let byDotted = Controller.bindingsByDotted[this.dotted];
+
+            if (!byDotted) {
+                byDotted = {
+                    dotted: this.dotted,
+                    bindings: [],
+                };
+
+                Controller.bindingsByDotted[this.dotted] = byDotted;
+            }
+
+            byDocElement.bindings.push(this);
+            byDotted.bindings.push(this);
+            return this;
+        }
+        
+        return null;
+    }
+
+    delete() {
+        // *****************************************************************************
+        // *****************************************************************************
+    }
+
+    disable() {
+        this.enabled = false;
+        return this;
+    }
+
+    enable() {
+        this.enabled = true;
+        return this;
+    }
+
+    static get(controllerBinding) {
+        if (controllerBinding.valid) {
+            if (controllerBinding.dotted in Controller.bindingsByDotted) {
+                let byDotted = Controller.bindingsByDotted[controllerBinding.dotted];
+
+                for (let binding of byDotted.bindings) {
+                    if (binding.docElement.isSame(controllerBinding.docElement)) {
+                        if (binding.type == controllerBinding.type) {
+                            if (binding.type == 'inner') {
+                                return binding;
+                            }
+
+                            if (binding.type == controllerBinding.type) {
+                                if (binding.name == controllerBinding.name) {
+                                    return binding;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    static has(controllerBinding) {
+        return ControllerBinding.get(controllerBinding) != null;
+    }
+
+    isDisabled() {
+        return !this.enabled;
+    }
+
+    isEnabled() {
+        return this.enabled;
     }
 });
